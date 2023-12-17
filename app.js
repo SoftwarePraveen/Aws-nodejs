807051bd8fe54363ecc85e0833c71c8ab22c72a5
expexpert <upworkwebexpert@gmail.com>
2023-12-17 13:27:36 +0530

    Let do work

--- app.js
+++ app.js
@@ -17,7 +17,20 @@ app.get("/check",async(req,res,next)=>{
         next(err)
     }
 })
-
+app.get("/hello",async(req,res,next)=>{
+    try{
+        res.send({message:"Hello Nodejs-aws is working fine or not"})
+    }catch(err){
+        next(err)
+    }
+})
+app.get("/",async(req,res,next)=>{
+    try{
+        res.send({message:"Hope it will work fine"})
+    }catch(err){
+        next(err)
+    }
+})
 app.listen(8000,()=>{
     console.log(`Listening at ${PORT}`);
 })
